c403ab51cf8d4c6a404a33b161a40857bc9f64f8
beltranbulbarella <beltran.bulbarella@ing.austral.edu.ar>
2023-03-29 12:00:16 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import {auth} from "@clerk/nextjs/app-beta";
+import { SignedIn, SignedOut } from "@clerk/nextjs/app-beta";
+// import { SignedIn, SignedOut } from "@clerk/nextjs/app-beta/client";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
